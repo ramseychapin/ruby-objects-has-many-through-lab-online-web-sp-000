bbd19fe08aa42a549659425127c4ea0ece3050af
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-05 18:27:18 +0000

    Automatically backed up by Learn

--- spec/01_artist_spec.rb
+++ spec/01_artist_spec.rb
@@ -48,9 +48,9 @@ describe "Artist" do
       jay_z = Artist.new("Jay-Z")
       rap = Genre.new("rap")
       ninety_nine_problems = jay_z.new_song("Ninety Nine Problems", rap)
-      binding.pry
+      #binding.pry
       expect(jay_z.genres).to include(rap)
-      expect(jay_z.songs.first.genre).to eq(rap)
+      expect(jay_z.songs.genre).to eq(rap)
     end
   end
 
