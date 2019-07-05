092b2d6270641492bc27fbe0c04d7d218cce4bb7
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-05 17:36:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,11 +5,11 @@ class Song
 
   def initialize(name, artist, genre)
     @name = name
-    @artist = artist
-    @genre = genre
+
+    
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
